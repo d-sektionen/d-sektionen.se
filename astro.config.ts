998f054bd62e7842355fb919bc5903d4c815f6bd
Otto Roming <otro@lysator.liu.se>
2026-09-15 02:12:10 +0200

    fixed tite; moved the public dir; fixed favicon

--- astro.config.ts
+++ astro.config.ts
@@ -1,28 +1,30 @@
 // @ts-check
-import { defineConfig, fontProviders } from 'astro/config';
+import { defineConfig, fontProviders } from "astro/config";
 
-import tailwindcss from '@tailwindcss/vite';
-import mdx from '@astrojs/mdx';
+import tailwindcss from "@tailwindcss/vite";
+import mdx from "@astrojs/mdx";
 
 // https://astro.build/config
 export default defineConfig({
   vite: {
-    plugins: [tailwindcss()]
+    plugins: [tailwindcss()],
   },
 
   integrations: [mdx()],
 
+  publicDir: "./src/data/public",
+
   fonts: [
     {
       provider: fontProviders.local(),
-      name: 'Rocky AOE',
-      cssVariable: '--font-rocky-aoe',
+      name: "Rocky AOE",
+      cssVariable: "--font-rocky-aoe",
       options: {
         variants: [
           {
-            src: ['./src/assets/fonts/ROCKYAOE.ttf'],
-            weight: 'normal',
-            style: 'normal',
+            src: ["./src/assets/fonts/ROCKYAOE.ttf"],
+            weight: "normal",
+            style: "normal",
           },
         ],
       },
